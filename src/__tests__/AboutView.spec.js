4abff1b04f123f1adc5b126e687d3c10f936efda
egarciav-dev-fe <egarciav.dev.fe@gmail.com>
2024-10-03 11:15:26 -0300

    prueba unitaria a la ruta about

--- src/__tests__/AboutView.spec.js
+++ src/__tests__/AboutView.spec.js
@@ -1,11 +1,38 @@
 import { describe, it, expect } from 'vitest'
 import { mount } from '@vue/test-utils'
 import AboutView from '@/views/AboutView.vue'
+import { createRouter, createWebHistory } from 'vue-router'
+import App from '@/App.vue'
 
 describe('AboutView', () => {
   it('snapshot de HTML', () => {
     const wrapper = mount(AboutView)
 
     expect(wrapper.html()).toMatchSnapshot()
+  })
+
+  it('Muestra la vista About mediante ruta', async () => {
+    const routerPrueba = createRouter({
+      history: createWebHistory(),
+      routes: [
+        {
+          path: '/about',
+          name: 'about',
+          component: AboutView
+        }
+      ]
+    })
+
+    const wrapper = mount(App, {
+      global: {
+        plugins: [routerPrueba]
+      }
+    })
+
+    routerPrueba.push({ name: 'about' })
+
+    await routerPrueba.isReady()
+
+    expect(wrapper.findComponent(AboutView).exists()).toBeTruthy()
   })
 })
